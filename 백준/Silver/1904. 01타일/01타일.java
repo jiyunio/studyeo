a499acd6a82d94f519b417cf110a5c5601ac10a5
jiyunio <146628970+jiyunio@users.noreply.github.com>
2024-07-16 09:45:10 +0900

    [Silver III] Title: 01타일, Time: 232 ms, Memory: 25488 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        long[] lst = new long[n + 1];
+        lst[1] = 1;
+        if (n == 1) {
+            System.out.println(lst[n]);
+            return;
+        }
+
+        lst[2] = 2;
+
+        for (int i = 3; i < n + 1; i++) {
+            lst[i] = (lst[i - 1] + lst[i - 2]) % 15746;
+        }
+        System.out.println(lst[n]);
+    }
+}
