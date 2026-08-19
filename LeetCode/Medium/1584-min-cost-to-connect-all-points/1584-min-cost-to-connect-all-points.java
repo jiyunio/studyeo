e8bcfb9b2858804f3039165ce92a86a7261c4d86
Jiyun Go <jiyun3269@naver.com>
2026-08-19 17:49:43 +0900

    Time: 17 ms (98.52%), Space: 45.2 MB (82.03%) - LeetHub

--- LeetCode/Medium/1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ LeetCode/Medium/1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,87 +1,45 @@
 import java.util.*;
-import java.io.*;
-
-/*
-모든 정점에 대해 가중치가 없으니까 
-가중치를 모든 가중치를 구한 뒤, 그래프 MST 찾기
-*/
-
-class Node {
-    int x;
-    int y;
-    String p;
-    int dist;
-    
-    Node(int x, int y, String p) {
-        this.x = x;
-        this.y = y;
-        this.p = p;
-        this.dist = 0;
-    }
-
-    Node(int x, int y, String p, int dist) {
-        this.x = x;
-        this.y = y;
-        this.p = p;
-        this.dist = dist;
-    }
-
-}
 
 class Solution {
-    HashMap<String, ArrayList<Node>> graph;
     public int minCostConnectPoints(int[][] points) {
-        graph = new HashMap<>();
-        init(points);
+        int n = points.length;
 
-        PriorityQueue<Node> q = new PriorityQueue<>((n1, n2) -> Long.compare(n1.dist, n2.dist)); //가중치를 기준으로 정렬
-        HashSet<String> visited = new HashSet<>();
-        q.add(new Node(points[0][0], points[0][1], (points[0][0] + "," + points[0][1])));
+        int[] minDist = new int[n];
+        boolean[] visited = new boolean[n];
+
+        Arrays.fill(minDist, Integer.MAX_VALUE);
+
+        minDist[0] = 0;
+
         int result = 0;
 
-        while(!q.isEmpty()) {
-            Node node = q.poll();
+        for (int i = 0; i < n; i++) {
 
-            if(visited.contains(node.p)) continue;
+            // 아직 방문하지 않은 정점 중
+            // MST에 연결하는 비용이 가장 작은 정점 선택
+            int cur = -1;
 
-            visited.add(node.p);
-            result += node.dist;
-            
-            if(visited.size() == points.length) break;
+            for (int j = 0; j < n; j++) {
+                if (!visited[j] && (cur == -1 || minDist[j] < minDist[cur])) {
+                    cur = j;
+                }
+            }
 
-            for(Node n : graph.get(node.p)) {
-                if(!visited.contains(n.p)) {
-                    //맨해튼 거리 계산 후 양방향 연결 (거리 == 가중치)
-                    int dist = Math.abs(node.x - n.x) + Math.abs(node.y - n.y);
-                    q.add(new Node(n.x, n.y, n.p, dist));
+            visited[cur] = true;
+            result += minDist[cur];
+
+            // 현재 정점과 다른 정점 사이의 거리로
+            // 최소 연결 비용 갱신
+            for (int next = 0; next < n; next++) {
+                if (!visited[next]) {
+                    int dist = Math.abs(points[cur][0] - points[next][0])
+                             + Math.abs(points[cur][1] - points[next][1]);
+
+                    minDist[next] = Math.min(minDist[next], dist);
                 }
             }
         }
 
         return result;
-    }
-
-    void init(int[][] points) {
-        for(int i = 0; i<points.length; i++) {
-            int s_x = points[i][0];
-            int s_y = points[i][1];
-            String s_p = s_x + "," + s_y;
-            graph.put(s_p, graph.getOrDefault(s_p, new ArrayList<>()));
-
-            for(int j = 0; j<points.length; j++) {
-                if(i == j) continue;
-                int e_x = points[j][0];
-                int e_y = points[j][1];
-
-                String e_p = e_x + "," + e_y;
-                ArrayList<Node> lst1 = graph.get(s_p);
-                ArrayList<Node> lst2 = graph.getOrDefault(e_p, new ArrayList<>());
-                lst1.add(new Node(e_x, e_y, e_p));
-                lst2.add(new Node(s_x, s_y, s_p));
-
-                graph.put(s_p, lst1);
-                graph.put(e_p, lst2);
-            }
-        }
     }
 }
