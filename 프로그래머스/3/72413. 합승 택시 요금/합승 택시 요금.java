6e028c9aab901822e0692ffdb21031b4d718099d
Jiyun Go <jiyun3269@naver.com>
2026-08-24 16:14:03 +0900

    [level 3] Title: 합승 택시 요금, Time: 42.36 ms, Memory: 68.4 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -1,91 +1,51 @@
 import java.util.*;
 
-/*
-최소 택시요금 구하기
-합승하고 내린 뒤 따로 가기
-처음부터 따로 가기
-합승 유무
-
-다익스트라
-시작위치에서 목적지까지 최단 경로를 구하는 알고리즘
-
-S에서 A와 B에 모두 가야 함 / 목적지가 두 개
-
-택시 내리는 것에서 다익스트라
-A의 다익스트라 + B의 다익스트라
-
-*/
-
-class Edge implements Comparable<Edge> {
-    int v;
-    int w;
-    
-    Edge(int v, int w) {
-        this.v = v;
-        this.w = w;
-    }
-    
-    @Override
-    public int compareTo(Edge e) {
-        return this.w - e.w;
-    }
-}
-
 class Solution {
-    ArrayList<Edge>[] graph;
+    static final int INF = 0x7FFFFFFF;
+    static final int SIZE = 201;
+
+    static int[][] adjMat = new int[SIZE][SIZE];
+    static int ans;
+
+    static void initGlobal() {
+        for (int i = 0; i < SIZE; i++) {
+            Arrays.fill(adjMat[i], INF);
+        }
+    }
+
     public int solution(int n, int s, int a, int b, int[][] fares) {
-        init(n, fares);
-        
-        int answer = Integer.MAX_VALUE;
-        for(int i = 1; i<n+1; i++) {
-            int taxi_cost = dijkstra(s, i, n);
-            int a_cost = dijkstra(i, a, n);
-            int b_cost = dijkstra(i, b, n);
-            
-            answer = Math.min(answer, (taxi_cost + a_cost + b_cost));
+        initGlobal();
+
+        for (int i = 0; i < fares.length; i++) {
+            adjMat[fares[i][0]][fares[i][1]] = fares[i][2];
+            adjMat[fares[i][1]][fares[i][0]] = fares[i][2];
         }
-        
-        return answer;
-    }
-    
-    void init(int n, int[][] fares) { //그래프 초기화 함수
-        graph = new ArrayList[n+1];
-        
-        for(int i = 0; i<n+1; i++) {
-            graph[i] = new ArrayList<>();
+
+        for (int i = 0; i <= n; i++) {
+            adjMat[i][i] = 0;
         }
-        
-        for(int i = 0; i<fares.length; i++) {
-            int s = fares[i][0];
-            int e = fares[i][1];
-            int w = fares[i][2];
-            
-            graph[s].add(new Edge(e, w));
-            graph[e].add(new Edge(s, w));
-        }
-    }
-    
-    int dijkstra(int start, int end, int n) {
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        pq.add(new Edge(start, 0));
-        int[] dist = new int[n+1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[start] = 0;
-        
-        while(!pq.isEmpty()) {
-            Edge edge = pq.poll();
-            
-            if(edge.v == end) break;
-            if(dist[edge.v] < edge.w) continue;
-            
-            for(Edge e : graph[edge.v]) {
-                if(dist[e.v] > edge.w + e.w) {
-                    dist[e.v] = edge.w + e.w;
-                    pq.add(new Edge(e.v, dist[e.v]));
+
+        // Floyd-Warshall
+        for (int mid = 1; mid <= n; mid++) {
+            for (int row = 1; row <= n; row++) {
+                for (int col = 1; col <= n; col++) {
+                    if (row == col) continue;
+                    if (adjMat[row][mid] == INF) continue;
+                    if (adjMat[mid][col] == INF) continue;
+
+                    int calc = adjMat[row][mid] + adjMat[mid][col];
+                    adjMat[row][col] = Math.min(adjMat[row][col], calc);
                 }
             }
         }
-        
-        return dist[end];
+
+        ans = adjMat[s][a] + adjMat[s][b];
+
+        for (int i = 1; i <= n; i++) {
+            int amount = adjMat[s][i] + adjMat[i][a] + adjMat[i][b];
+            ans = Math.min(ans, amount);
+        }
+
+        return ans;
     }
 }
