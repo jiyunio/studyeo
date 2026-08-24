a3ca311ab0dffc8218c1cd04e76861fd687c4c08
Jiyun Go <jiyun3269@naver.com>
2026-08-24 13:43:53 +0900

    [level 3] Title: 등산코스 정하기, Time: 78.50 ms, Memory: 152 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
@@ -1,13 +1,5 @@
 import java.util.*;
 
-/*
-다익스트라는 최단경로를 만드는 것
-이것는 최소 intensity를 만드는 것
-
-해당 경로까지 갔을 때, intensity가 최소가 되는 경로로 가면 됨
-total은 신경쓰지 않아도 된다.
-*/
-
 class Edge implements Comparable<Edge> {
     int v;
     int w;
@@ -28,13 +20,12 @@ class Edge implements Comparable<Edge> {
 
 class Solution {
     public int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
-        int[] answer = new int[2];
-        Arrays.fill(answer, Integer.MAX_VALUE);
-        
+        //봉우리 set
         HashSet<Integer> summit = new HashSet<>();
         for(int s : summits) {
             summit.add(s);
         }
+        //출입구 set
         HashSet<Integer> gate = new HashSet<>();
         for(int g : gates) {
             gate.add(g);
@@ -54,48 +45,38 @@ class Solution {
             graph[e].add(new Edge(s, w));
         }
         
+        //결과큐
         PriorityQueue<Edge> result = new PriorityQueue<>();
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        int[] dist = new int[n+1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        
         for(int g : gates) {
-            //pq : 우선순위큐 (가중치에 대해 오름차순 정렬)
-            PriorityQueue<Edge> pq = new PriorityQueue<>();
             pq.add(new Edge(g, 0));
-            
-            //dist : [n]까지 갔을 때의 최소 intensity (intensity : 경로 중에서 최댓값)
-            int[] dist = new int[n+1];
-            Arrays.fill(dist, Integer.MAX_VALUE);
             dist[g] = 0;
-            
-            while(!pq.isEmpty()) {
-                Edge edge = pq.poll();
-                
-                if(dist[edge.v] < edge.w) continue; //더 큰 가중치 경로는 갈 필요없음
-                if(summit.contains(edge.v)) {
-                    result.add(edge);
-                    break;
-                }
-                
-                for(Edge e : graph[edge.v]) { //주변 정점
-                    if(dist[e.v] > Math.max(edge.w, e.w) && !gate.contains(e.v)) {
-                        dist[e.v] = Math.max(edge.w, e.w);
-                        pq.add(new Edge(e.v, dist[e.v]));
-                        //System.out.printf("edge.v : %d | e.v : %d | e.w : %d | dist[e.v] : %d\n", edge.v, e.v, e.w, dist[e.v]);
-                    }
-                }
-            }
-            
-            // //각 출입구에 대해 answer 갱신
-            // for(int s : summits) { 
-            //     if(answer[0] == s) { //산봉우리가 같을 때
-            //         answer[1] = Math.min(answer[1], dist[s]); //intensity 최솟값으로 갱신
-            //     } else { //산봉우리가 다를 때
-            //         if(answer[1] > dist[s]) { //intensity 최솟값 갱신이 가능할 때
-            //             answer[0] = s;
-            //             answer[1] = dist[s];
-            //         }
-            //     }
-            // }
         }
         
+        
+        
+        while(!pq.isEmpty()) {
+            Edge edge = pq.poll();
+            
+            if(dist[edge.v] < edge.w) continue; //더 큰 가중치 경로는 갈 필요없음
+            if(summit.contains(edge.v)) { //봉우리 도착 | intensity가 가장 작은 경로니까 바로 break
+                result.add(edge);
+                continue;
+            }
+            
+            for(Edge e : graph[edge.v]) { //주변 정점
+                if(dist[e.v] > Math.max(edge.w, e.w) && !gate.contains(e.v)) {
+                    dist[e.v] = Math.max(edge.w, e.w);
+                    pq.add(new Edge(e.v, dist[e.v]));
+                }
+            }
+        }
+        
+        
+        int[] answer = new int[2];
         Edge e = result.poll();
         answer[0] = e.v;
         answer[1] = e.w;
